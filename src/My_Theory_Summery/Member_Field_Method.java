42ee9a300ea9e72b07c113d509586df870bc99ac
jw <malinup@naver.com>
2023-03-15 09:29:38 +0900

    commit

---
+++ src/My_Theory_Summery/Member_Field_Method.java
@@ -0,0 +1,47 @@
+package My_Theory_Summery;
+
+
+
+public class Member_Field_Method {
+
+//    Java에서 멤버 필드와 멤버 메소드는 객체지향 프로그래밍에서 가장 기본적인 개념 중 하나입니다.
+//    멤버 필드는 클래스 내에 선언된 변수를 의미합니다.
+//    이 변수는 해당 클래스의 인스턴스가 생성될 때마다 메모리에 할당되며,
+//    인스턴스 내에서 사용할 수 있습니다. 멤버 필드는 클래스의 속성을 나타내며,
+//    클래스의 인스턴스가 가지는 고유한 상태를 나타내는 역할을 합니다.
+//    예를 들어, 다음과 같이 Member_Field_Method 클래스를 정의할 수 있습니다.
+
+//    private String name;
+//    private int age;
+
+//    위 코드에서 name과 age는 멤버 필드입니다. 이러한 멤버 필드는 Member_Field_Method 클래스의 모든 인스턴스에서 공유됩니다.
+//    멤버 메소드는 클래스 내에 선언된 함수를 의미합니다.
+//    Member 클래스에 다음과 같은 메소드를 추가할 수 있습니다.
+
+//    private String name;
+//    private int age;
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public String getName() {
+//        return this.name;
+//    }
+//
+//    public int getAge() {
+//        return this.age;
+//    }
+//    위 코드에서 setName, setAge, getName, getAge는 모두 멤버 메소드입니다.
+//    이러한 멤버 메소드는 Member_Field_Method 클래스의 인스턴스에서 호출할 수 있으며,
+//    해당 인스턴스의 상태를 변경하거나 조회할 수 있습니다.
+//    요약하자면, 멤버 필드는 클래스의 속성을 나타내며,
+//    멤버 메소드는 클래스의 행동을 정의합니다. 클래스의 인스턴스는 멤버 필드를 통해 고유한 상태를 가지며,
+//    멤버 메소드를 통해 동작합니다.
+//
+//
+}
--- src/My_Try2/AddZeroException.java
+++ src/My_Try2/AddZeroException.java
@@ -2,6 +2,7 @@ package My_Try2;
 
 public class AddZeroException extends Exception {
     AddZeroException(String msg) {
+
         super(msg);
     }
 }
