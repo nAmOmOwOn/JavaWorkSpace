d97270e2300cfbb1d968c88daddb21a86549299f
jw <malinup@naver.com>
2023-03-22 17:27:23 +0900

    commit

---
+++ src/My_Theory_Summery/Static.java
@@ -0,0 +1,20 @@
+package My_Theory_Summery;
+
+public class Static {
+//    static , 모든 객체가 공유, 클래스 이름으로 접근 가능
+
+//    class Car{
+//        static String name; // 공유, 클래스이름 또는 객체참조변수로 접근 가능
+//        int age; // 인스턴스 맴버
+//    }
+//
+//    Car c1 = new Car();
+//    c1.name = "홍길동";
+//    Car c2 = new Car();
+//    c2.name = "강감찬";
+//
+//    System.out.println(c1.name); //강감찬
+//    System.out.println(c2.name); //강감찬
+//    System.out.println(Car.name);//강감찬
+//    }
+}
